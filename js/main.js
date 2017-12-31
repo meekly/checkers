fbde7e96caf7afb327c4c830eb27db600ca29ea2
Valentine <mrexox@yahoo.com>
2017-12-31 09:10:48 +0300

    dummy

--- js/main.js
+++ js/main.js
@@ -8,3 +8,47 @@ document.addEventListener("DOMContentLoaded", function(){
 
 		bindEvents(); // Обработчики нажатий на кнопки
 });
+
+
+// HELPERS
+
+function ajax(method, url, data, success, error) {
+		// Success and Error callbacks
+		if (success === undefined || success == null) {
+				success = function(data) {
+						console.log("Recieved with ajax:\n"+data);
+				};
+		}
+		if (error === undefined || error == null) {
+				error = function(data) {
+						console.log("Error in ajax:\n"+data);
+				}
+		}
+		
+		// Matching methods
+		var getRe = new RegExp("GET", "i");
+		var postRe = new RegExp("POST", "i");
+
+		// Callbacks settings
+		var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+						if (xmlhttp.status >= 200 && xmlhttp.status <= 299) {
+								success(xmlhttp.responseText);
+						}
+						else {
+								error(xmlhttp.responseText);
+						}
+        }
+    };
+
+   	// Sending
+		if (getRe.test(method)) {		// GET
+				xmlhttp.open("GET", url, true);
+				xmlhttp.send();
+		}
+		else if (postRe.test(method)) { // POST
+				xmlhttp.open("POST", url, true);		
+				xmlhttp.send(data);
+		}
+}
